3eec45e63a1ab850c015db218527631d8e073f89
DenTjorven <103747429+DenTjorven@users.noreply.github.com>
2023-03-22 15:16:17 +0100

    Delete test.ts

--- test.ts
+++
@@ -1,8 +0,0 @@
-const fetcher = require('node-fetch');
-const doFetch = async ():Promise<void> =>{
-    let result:any = await fetcher(`https://pokeapi.co/api/v2/pokemon/1`);
-    let response:any = await result.json();
-    console.log(response);
-}
-doFetch();
-export{}
